07998c686dd4c544fbe423e166d8b3c33c31cd7d
Viking Jonsson <Viking.Jonsson@nordnet.se>
2020-07-11 22:40:25 +0200

    feat: add main

--- main.lua
+++ main.lua
@@ -2,10 +2,9 @@ if os.getenv('LOCAL_LUA_DEBUGGER_VSCODE') == '1' then
   require('lldebugger').start()
 end
 
-Class = require 'lib/hump/class'
-local push = require 'lib/push.push'
-require 'Ball'
-require 'Paddle'
+local push = require 'lib.push.push'
+local Ball = require 'Ball'
+local Paddle = require 'Paddle'
 
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
@@ -17,7 +16,7 @@ local function rgba(red, green, blue, alpha)
 end
 
 local player1, player2, ball
-local smallFont, scoreFont
+local small_font, score_font
 local players = {}
 local sounds = {}
 local scores = {
@@ -34,14 +33,14 @@ STATES = {
 
 GAME_STATE = STATES.START
 
-local servingPlayer = 1
+local serving_player = 1
 
 function love.load()
   math.randomseed(os.time())
   love.graphics.setDefaultFilter('nearest', 'nearest')
   love.window.setTitle('Pong!')
-  smallFont = love.graphics.newFont('font/8bit16.ttf', 8)
-  scoreFont = love.graphics.newFont('font/8bit16.ttf', 32)
+  small_font = love.graphics.newFont('font/8bit16.ttf', 8)
+  score_font = love.graphics.newFont('font/8bit16.ttf', 32)
 
   sounds = {
     paddle_hit = love.audio.newSource('sounds/paddle_hit.wav', 'static'),
@@ -49,9 +48,13 @@ function love.load()
     wall_hit = love.audio.newSource('sounds/wall_hit.wav', 'static')
   }
 
+  ---@type Ball
   ball = Ball(VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4)
+  ---@type Paddle
   player1 = Paddle(10, 30, 5, 20, {up = 'w', down = 's'})
+  ---@type Paddle
   player2 = Paddle(VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 50, 5, 20, {up = 'o', down = 'l'})
+  ---@type Paddle[]
   players = {player1, player2}
 
   push:setupScreen(
@@ -73,7 +76,7 @@ function love.update(dt)
     ball:reset()
 
     ball.dy = math.random(-50, 50)
-    if servingPlayer == 1 then
+    if serving_player == 1 then
       ball.dx = math.random(140, 200)
     else
       ball.dx = -math.random(140, 200)
@@ -107,7 +110,7 @@ function love.update(dt)
     end
 
     for index, player in ipairs(players) do
-      if ball:onCollision(player) then
+      if ball:on_collision(player) then
         sounds.paddle_hit:play()
         ball.x = index == 1 and player.x + player.width or player.x - ball.width
         ball.dx = -ball.dx * 1.05
@@ -117,7 +120,7 @@ function love.update(dt)
 end
 
 function love.draw()
-  push:apply('start')
+  push:start()
   love.graphics.clear(rgba(40, 45, 52, 1))
 
   -- game play
@@ -126,27 +129,27 @@ function love.draw()
   ball:draw()
 
   -- HUD
-  love.graphics.setFont(scoreFont)
+  love.graphics.setFont(score_font)
   love.graphics.print(tostring(scores.p1), VIRTUAL_WIDTH / 2 - 50, VIRTUAL_HEIGHT / 3)
   love.graphics.print(tostring(scores.p2), VIRTUAL_WIDTH / 2 + 30, VIRTUAL_HEIGHT / 3)
 
-  love.graphics.setFont(smallFont)
+  love.graphics.setFont(small_font)
   if GAME_STATE == STATES.SERVE then
-    love.graphics.printf('Player ' .. tostring(servingPlayer) .. ' ' .. GAME_STATE, 0, 20, VIRTUAL_WIDTH, 'center')
+    love.graphics.printf('Player ' .. tostring(serving_player) .. ' ' .. GAME_STATE, 0, 20, VIRTUAL_WIDTH, 'center')
   elseif GAME_STATE == STATES.DONE then
-    local winningMessage
+    local winning_message = ''
     if scores.p1 == 10 then
-      winningMessage = 'Player 1 '
+      winning_message = 'Player 1 '
     else
-      winningMessage = 'Player 2'
+      winning_message = 'Player 2'
     end
 
-    love.graphics.printf(winningMessage .. 'Wins', 0, 20, VIRTUAL_WIDTH, 'center')
+    love.graphics.printf(winning_message .. 'Wins', 0, 20, VIRTUAL_WIDTH, 'center')
   else
     love.graphics.printf(GAME_STATE, 0, 20, VIRTUAL_WIDTH, 'center')
   end
 
-  push:apply('end')
+  push:finish()
 end
 
 function love.keypressed(key)
@@ -157,7 +160,7 @@ function love.keypressed(key)
   if key == 'return' and GAME_STATE == STATES.START then
     GAME_STATE = STATES.SERVE
   elseif key == 'return' and GAME_STATE == STATES.SERVE then
-    servingPlayer = servingPlayer == 1 and 2 or 1
+    serving_player = serving_player == 1 and 2 or 1
     GAME_STATE = STATES.PLAY
   elseif key == 'return' and GAME_STATE == STATES.DONE then
     scores.p1 = 0

10a40b6274c7f189901279251512a324073929fd
Viking Jonsson <Viking.Jonsson@nordnet.se>
2020-07-11 22:39:43 +0200

    feat: add ball class

--- Ball.lua
+++ Ball.lua
@@ -1,12 +1,22 @@
-Ball = Class {}
+local Class = require 'lib.hump.class'
 
-local function checkCollisionAABB(a, b)
+--region Ball
+---@class Ball
+local Ball = Class {}
+
+---@param a table<string, number>
+---@param b table<string, number>
+local function check_collision_AABB(a, b)
   if a.x < b.x + b.width and a.y < b.y + b.height and b.x < a.x + a.width and b.y < a.y + a.height then
     return true
   end
   return false
 end
 
+---@param x number
+---@param y number
+---@param width number
+---@param height number
 function Ball:init(x, y, width, height)
   self.x = x
   self.y = y
@@ -16,21 +26,23 @@ function Ball:init(x, y, width, height)
   self.dy = 0
 end
 
+---@param dt number
 function Ball:update(dt)
   self.x = self.x + self.dx * dt
   self.y = self.y + self.dy * dt
 
-  local touchedBottomEade = self.y + self.height > VIRTUAL_HEIGHT
-  self.dy = touchedBottomEade and -self.dy or self.dy
-  self.y = touchedBottomEade and VIRTUAL_HEIGHT - self.height or self.y
+  local touch_bottom = self.y + self.height > VIRTUAL_HEIGHT
+  self.dy = touch_bottom and -self.dy or self.dy
+  self.y = touch_bottom and VIRTUAL_HEIGHT - self.height or self.y
 
-  local touchedTopEdge = self.y < 0
-  self.dy = touchedTopEdge and -self.dy or self.dy
-  self.y = touchedTopEdge and 0 or self.y
+  local touched_top = self.y < 0
+  self.dy = touched_top and -self.dy or self.dy
+  self.y = touched_top and 0 or self.y
 end
 
-function Ball:onCollision(collider)
-  return checkCollisionAABB(self, collider)
+---@param collider Paddle
+function Ball:on_collision(collider)
+  return check_collision_AABB(self, collider)
 end
 
 function Ball:draw()
@@ -43,3 +55,6 @@ function Ball:reset()
   self.dx = 0
   self.dy = 0
 end
+
+return Ball
+--endregion Ball
